7680800a85132fb75410e190c3574e643b1621a0
Rirapp <arirafkarhala.ms@gmail.com>
2023-06-02 00:56:31 +0700

    fixing date

--- practicalTest/src/ProfileApp.java
+++ practicalTest/src/ProfileApp.java
@@ -1,71 +1,97 @@
-//import java.io.File;
-//import java.io.FileWriter;
-//import java.io.IOException;
-//import java.nio.file.Files;
-//import java.nio.file.Paths;
-//import java.util.ArrayList;
-//import java.util.Date;
-//import java.util.List;
-//
-//class Person {
-//    String name;
-//    Date tanggalLahir;
-//    Integer nik;
-//
-//    public Person(String name, Date tanggalLahir, Integer nik) {
-//        this.name = name;
-//        this.tanggalLahir = tanggalLahir;
-//        this.nik = nik;
-//    }
-//}
-//
-//public class ProfileApp {
-//    public static void main(String[] args) {
-//        // Step 1: Memasukkan objek ke dalam array of objects
-//        List<Person> persons = new ArrayList<>();
-//        persons.add(new Person("Budi", new Date(99, 0, 1), 123456789));
-//        persons.add(new Person("Wati", new Date(99, 11, 31), 987654321));
-//
-//        // Step 2: Menulis data dalam array ke dalam file profile.txt
-//        writeToFile(persons, "profile.txt");
-//
-//        // Step 3: Membaca isi file profile.txt dan memasukkan hanya Nama ke dalam array of strings
-//        String[] names = readNamesFromFile("profile.txt");
-//
-//        // Menampilkan output array of strings (nama)
-//        for (String name : names) {
-//            System.out.println(name);
-//        }
-//    }
-//
-//    private static void writeToFile(List<Person> persons, String filename) {
-//        try {
-//            FileWriter writer = new FileWriter(filename);
-//            for (Person person : persons) {
-//                String line = person.nik + "," + person.name + "," + person.tanggalLahir.toString();
-//                writer.write(line + "\n");
-//            }
-//            writer.close();
-//            System.out.println("Data berhasil ditulis ke dalam file " + filename);
-//        } catch (IOException e) {
-//            System.out.println("Terjadi kesalahan dalam penulisan file.");
-//            e.printStackTrace();
-//        }
-//    }
-//
-//    private static String[] readNamesFromFile(String filename) {
-//        try {
-//            List<String> lines = Files.readAllLines(Paths.get(filename));
-//            String[] names = new String[lines.size()];
-//            for (int i = 0; i < lines.size(); i++) {
-//                String[] parts = lines.get(i).split(",");
-//                names[i] = parts[1];
-//            }
-//            return names;
-//        } catch (IOException e) {
-//            System.out.println("Terjadi kesalahan dalam membaca file.");
-//            e.printStackTrace();
-//            return new String[0];
-//        }
-//    }
-//}
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.util.*;
+
+
+class Person {
+    String name;
+    LocalDate tanggalLahir;
+    int nik;
+
+    public Person(String name, LocalDate tanggalLahir, int nik) {
+        this.name = name;
+        this.tanggalLahir = tanggalLahir;
+        this.nik = nik;
+    }
+}
+
+
+public class ProfileApp {
+    public static void main(String[] args) {
+        List<Person> persons = readPersonsFromInput();
+        writeToFile(persons, "profile.txt");
+        String[] names = readNamesFromFile("profile.txt");
+
+        for (String name : names) {
+            System.out.println(name);
+        }
+    }
+
+    private static List<Person> readPersonsFromInput() {
+        List<Person> persons = new ArrayList<>();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+
+        try {
+            while (true) {
+                System.out.print("Masukkan Nama (atau tekan Enter untuk selesai): ");
+                String name = reader.readLine();
+                if (name.isEmpty()) {
+                    break;
+                }
+
+                System.out.print("Masukkan Tanggal Lahir (Format: yyyy-MM-dd): ");
+                String tanggalLahirStr = reader.readLine();
+                LocalDate tanggalLahir = LocalDate.parse(tanggalLahirStr);
+
+                System.out.print("Masukkan NIK: ");
+                int nik = Integer.parseInt(reader.readLine());
+
+                Person person = new Person(name, tanggalLahir, nik);
+                persons.add(person);
+            }
+        } catch (IOException e) {
+            System.out.println("Terjadi kesalahan dalam membaca input.");
+            e.printStackTrace();
+        } catch (NumberFormatException e) {
+            System.out.println("NIK harus berupa angka.");
+            e.printStackTrace();
+        }
+
+        return persons;
+    }
+
+    private static void writeToFile(List<Person> persons, String filename) {
+        try {
+            FileWriter writer = new FileWriter(filename);
+            for (Person person : persons) {
+                String line = person.nik + "," + person.name + "," + person.tanggalLahir.toString();
+                writer.write(line + "\n");
+            }
+            writer.close();
+            System.out.println("Data berhasil dimasukan ke dalam file " + filename);
+        } catch (IOException e) {
+            System.out.println("Terjadi kesalahan dalam penulisan file.");
+            e.printStackTrace();
+        }
+    }
+
+    private static String[] readNamesFromFile(String filename) {
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(filename));
+            String[] names = new String[lines.size()];
+            for (int i = 0; i < lines.size(); i++) {
+                String[] parts = lines.get(i).split(",");
+                names[i] = parts[1];
+            }
+            return names;
+        } catch (IOException e) {
+            System.out.println("Terjadi kesalahan dalam membaca file.");
+            e.printStackTrace();
+            return new String[0];
+        }
+    }
+}
